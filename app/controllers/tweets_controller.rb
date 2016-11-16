9194adb125261df2e94ad9a692535bf9b21ffc74
Taichi Koido <taichi1009@gmail.com>
2016-11-17 00:55:13 +0900

    新規投稿機能の追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,19 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    # redirect_to root_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :text, :image)
   end
 end
