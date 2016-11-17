a7c75243ecef943b60081914c10552f25272fefe
Taichi Koido <taichi1009@gmail.com>
2016-11-17 12:11:15 +0900

    コメント機能を追加、部分テンプレートも追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: :show
 
   root 'tweets#index'
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: :create
+  end
 end
